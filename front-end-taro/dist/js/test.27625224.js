f1b02e4fb40fed57e519244b8922a23d417c1948
LevanDu <441489115@qq.com>
2025-10-24 10:49:09 +0800

    dist

---
+++ front-end-taro/dist/js/test.27625224.js
@@ -0,0 +1,1 @@
+import{j as s,V as e}from"./vendors.5898faf2.js";import{w as t}from"./common.70eecc84.js";const o=()=>{const t="\u6d4b\u8bd5\u9875";return s.jsxs(e,{className:"layout",children:[s.jsx(e,{style:"font-size:40rpx;",children:"I am a test Page"}),s.jsx(e,{style:"font-size:40rpx;",children:t})]})},r=t(o);export{r as default};
